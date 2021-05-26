2412f59bb38d5ba070b7d74dade1f96b1888e892
Jon Smith <aardvarkjon94@yahoo.com>
2021-05-26 17:03:52 -0500

    Refactored code

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -10,13 +10,37 @@ function App() {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+
+  const Header = ({course}) => {
+    return (<h1>{course}</h1>
+    )}
+
+  const Part = ({part, exercises}) => {
+    return (
+      <p>{part} {exercises}</p>
+    )
+  }
+
+    const Content = () => {
+      return(
+        <>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
+        </>
+      )}
+
+    
+
+const Total = ({total}) => {
+  return(
+<p>Number of exercises {total}</p>
+  )}
   return (
     <div className="App">
-      <h1>{course}</h1>
-      <p>{part1} {exercises1}</p>
-      <p>{part2}{exercises2}</p>
-      <p>{part3}{exercises3}</p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content />
+      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 }
