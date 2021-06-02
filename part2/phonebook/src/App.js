ba9666e496f6214f880e9e4a752bf0930b9bc36a
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-02 12:00:34 -0500

    Added post route to phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -51,9 +51,11 @@ const App = () => {
 
     } else {
       return (
-        setPersons(persons.concat(newPerson)),
-        setNewName(''),
-        setNewNumber('')
+        axios.post('http://localhost:3001/persons', newPerson)
+             .then(response => setPersons(persons.concat(response.data)),
+             setNewName(''),
+             setNewNumber(''))
+        
       )
       
     }
