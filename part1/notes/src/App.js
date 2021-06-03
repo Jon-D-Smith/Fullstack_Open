b3a1135f96128092cd4e5af2ae431c1587ff594e
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-03 17:24:16 -0500

    Changed catch method to display error message instead of alert

--- part1/notes/src/App.js
+++ part1/notes/src/App.js
@@ -1,12 +1,14 @@
 import React, {useState, useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
+import Notification from './components/Notification'
 
 const App = (props) => {
 
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -26,9 +28,12 @@ const App = (props) => {
         setNotes(notes.map(note => note.id !== id ? note : returnedNote))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from the server`
+        setErrorMessage(
+          `Note '${note.content}' no longer exists`
         )
+        setTimeout(()=>{
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(note => note.id !== id))
       })
     
@@ -68,6 +73,7 @@ const App = (props) => {
   return(
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={()=> setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button>
       </div>
