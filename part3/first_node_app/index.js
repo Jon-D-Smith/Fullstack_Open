a71551722369b4ac7fbe1ba39a0825902018a721
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-04 17:42:55 -0500

    Basic Hello world

--- part3/first_node_app/index.js
+++ part3/first_node_app/index.js
@@ -1,1 +1,11 @@
-console.log("Hello world")
+const http = require('http')
+
+const app = http.createServer((req, res) => {
+    res.writeHead(200, {'content-type': 'text/plain'})
+    res.end('Hello world')
+})
+
+const PORT = 3001
+
+app.listen(PORT)
+console.log(`Listening on port ${PORT}`)
