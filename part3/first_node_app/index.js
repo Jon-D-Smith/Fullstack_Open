579366c59c251ff1f98d9718f19e68243652a91e
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-04 17:52:20 -0500

    Transitioned app to an express app

--- part3/first_node_app/index.js
+++ part3/first_node_app/index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express= require('express')
+const app = express()
 
 let notes = [
     {
@@ -21,12 +22,16 @@ let notes = [
       }
 ]
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'content-type': 'application/json'})
-    res.end(JSON.stringify(notes))
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
 })
 
 const PORT = 3001
 
-app.listen(PORT)
-console.log(`Listening on port ${PORT}`)
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+})
