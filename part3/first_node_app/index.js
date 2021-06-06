f62b9377ff8c3f0c49a2e1e784f972f87dd63f06
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-06 14:07:56 -0500

    Linked backend and frontend; implimented cors middleware

--- part3/first_node_app/index.js
+++ part3/first_node_app/index.js
@@ -1,5 +1,6 @@
 const express= require('express')
 const app = express()
+const cors = require('cors')
 
 let notes = [
     {
@@ -23,6 +24,7 @@ let notes = [
 ]
 
 app.use(express.json())
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
