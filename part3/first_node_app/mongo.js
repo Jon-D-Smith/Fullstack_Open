7b8b05ee44e40a9dd3735c5b090f723a3aac16bb
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-07 12:47:52 -0500

    Connected notes to local mongoDB

---
+++ part3/first_node_app/mongo.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost:27017/notes-app', {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true});
+
+const noteSchema = new mongoose.Schema({
+content: String,
+date: Date,
+important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+const note = new Note({
+    content: 'HTML is easy',
+    date: new Date(),
+    important: true
+})
+
+note.save().then(result => {
+    console.log('note saved')
+    console.log(note)
+    mongoose.connection.close()
+})
