7349072a2b01940fa4fa1f0b321d8ca82491a232
Jon Smith <aardvarkjon94@yahoo.com>
2021-06-23 12:53:39 -0500

    Minor changes to login router

--- part3/first_node_app/controllers/login.js
+++ part3/first_node_app/controllers/login.js
@@ -2,6 +2,9 @@ const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
+require('dotenv').config()
+
+
 
 loginRouter.post('/', async (request, response) => {
     const body = request.body
